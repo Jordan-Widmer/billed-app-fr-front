fdb7b4e0ee1b4c42dbfba63b35e5ff28045ed408
Lord Shaxx <87854378+Korootak@users.noreply.github.com>
2023-01-08 18:14:25 +0100

    Bug report - Bills & Login | Bug Hunt - Bills & Dashboard
    Tests unitaires | Projet realiser en 9 jours.

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,18 +2,19 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import { fireEvent, screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import userEvent from '@testing-library/user-event'
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -25,13 +26,36 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+      expect(windowIcon.classList.contains('active-icon')).toBeTruthy();
     })
+    test("Should show the report when click on the eye button", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      let PREVIOUS_LOCATION = "";
+
+      const store = jest.fn();
+
+
+      const billss = new Bills({
+        document, onNavigate, store, localStorage
+      });
+      const eyeButtons = screen.getAllByTestId("icon-eye")[0];
+
+      const handleSubmit = jest.fn(() => billss.handleClickIconEye(eyeButtons));
+      eyeButtons.addEventListener('click', handleSubmit)
+      fireEvent.click(eyeButtons);
+      expect(handleSubmit).toHaveBeenCalled();
+    })
+
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const antiChrono = (a, b) => ((a - b) ? -1 : 1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,108 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, waitFor, fireEvent, user } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
-
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import router from "../app/Router.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import userEvent from '@testing-library/user-event'
+import { ROUTES } from "../constants/routes";
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then the new buill model should be visisble", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      expect(screen.getByTestId("expense-type")).toBeTruthy();
+    })
+
+    test("Then Message icon in vertical layout should be highlighted", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const windowIcon = screen.getByTestId('icon-mail')
+      expect(windowIcon.classList.contains('active-icon')).toBeTruthy();
     })
   })
+
+  describe("When I am on NewBill Page", () => {
+    test("Then the new bill should submit the value", () => {
+      const html = NewBillUI()
+      document.body.innerHTML = html
+
+      const Typededepense = screen.getByTestId("expense-type");
+      fireEvent.change(Typededepense, { target: { value: "Restaurants et bars" } });
+      expect(Typededepense.value).toBe("Restaurants et bars");
+
+
+      const expenseName = screen.getByTestId("expense-name");
+      fireEvent.change(expenseName, { target: { value: "test Expense" } });
+      expect(expenseName.value).toBe("test Expense");
+
+
+      const datepickerr = screen.getByTestId("datepicker");
+      expect(datepickerr).toBeTruthy()
+      fireEvent.click(datepickerr, { target: { value: '2020-01-01' } });
+      expect(datepickerr.value).toEqual('2020-01-01');
+
+      const amount = screen.getByTestId("amount");
+      fireEvent.change(amount, { target: { value: "123" } });
+      expect(amount.value).toBe("123");
+
+      const vat = screen.getByTestId("vat");
+      fireEvent.change(vat, { target: { value: "5" } });
+      expect(vat.value).toBe("5");
+
+
+      const pct = screen.getByTestId("pct");
+      fireEvent.change(pct, { target: { value: "12" } });
+      expect(pct.value).toBe("12");
+
+      const commentary = screen.getByTestId("commentary");
+      fireEvent.change(commentary, { target: { value: "hello commentary" } });
+      expect(commentary.value).toBe("hello commentary");
+
+      const someValues = [{ name: 'teresa teng' }];
+
+      const str = JSON.stringify(someValues);
+      const blob = new Blob([str]);
+      const file = new File([blob], 'values.json', {
+        type: 'application/JSON',
+      });
+      File.prototype.text = jest.fn().mockResolvedValueOnce(str);
+      const input = screen.getByTestId('file');
+
+
+      userEvent.upload(input, file);
+      waitFor(() => expect(screen.queryByTestId('input')).toBeTruthy());
+      const form = screen.getByTestId("form-new-bill");
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      let PREVIOUS_LOCATION = "";
+
+      const store = jest.fn();
+
+
+      const newBill = new NewBill({
+        document, onNavigate, store, localStorage
+      });
+
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+      newBill.updateBill = jest.fn().mockResolvedValue({});
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
 })
